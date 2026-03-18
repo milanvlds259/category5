5b523732f2ed044cc3d7fff3ce0e213d64f9e703
NiccoF <niccofaelnar@gmail.com>
2026-03-18 15:34:51 -0500

    Reduce crowding of paths
    - Added moving path entrances away from each other
    - Moving path midpoints away from each other
    - Still some problems listed at the top of MapGenerator

--- Assets/Scripts/Map/MapGenerator.cs
+++ Assets/Scripts/Map/MapGenerator.cs
@@ -2,21 +2,22 @@ using System;
 using System.Collections.Generic;
 using System.Collections;
 using Debug = UnityEngine.Debug;
-using Unity.VisualScripting;
+using Vector3 = UnityEngine.Vector3;
 using UnityEngine;
 using Random = UnityEngine.Random;
 using UnityEngine.Splines;
 using Unity.Mathematics;
 using System.Linq;
+using System.Numerics;
 
 public class MapGenerator : MonoBehaviour
 {
     // CURRENT ISSUES!
     /*
-        - Arenas can still be generated with some overlap. Like two might be in a row from another arena, but they're still
-        the closest 2, so the paths end up kind of overlapping (Delete paths that overlap? Chance to have unreachable arenas maybe)
-        - Paths Can get too close together (REJECTION CHECK. KEEP ALL OTHER POINTS TO CHECK FOR DISTANCE)
-        - Path entrances can get crowded, and maybe want some random not quite closest point entrances (Add random offset within += angle range of the closest point, then use raycast from inside the arena to the collider to get a point)
+        - Path entrances can cross over each other
+        - Paths can go over arenas
+        - Path spacing not working
+        - Want to make paths only move points side to side?
     */
 
 
@@ -40,12 +41,14 @@ public class MapGenerator : MonoBehaviour
     // Keep track of all path points to make sure they're not too close together
     private List<BezierKnot> pathMidpoints = new List<BezierKnot>();
 
+
     // Mesh to generate along paths
     [SerializeField] Mesh pathMesh;
 
     class Arena
     {
         public Vector3 position;
+        public float scaleFactor;
         public GameObject gameObjectRef;
 
         // Stores if this arena is an eye or not, players can drop into eyes
@@ -55,12 +58,15 @@ public class MapGenerator : MonoBehaviour
         public bool isHidden;
         // Arenas have a capsule collider surrounding them that defines
         // the arena's boundaries (The storm cloud walls)
-        public Collider arenaBounds;
+        public CapsuleCollider arenaBounds;
 
-        public Arena(Vector3 pos, GameObject objRef)
+
+        public Arena(Vector3 pos, GameObject objRef, float scale)
         {
             position = pos;
             gameObjectRef = objRef;
+            scaleFactor = scale;
+
             // Arenas are not eyes by default. If they are set to true it's in GenerateMap()
             isEye = false;
 
@@ -76,11 +82,15 @@ public class MapGenerator : MonoBehaviour
 
         public bool isHidden;
 
+        // The spline that makes up the physical object of this path
+        public Spline spline;
+
         public Path(Arena a, Arena b, GameObject objRef)
         {
             arenaA = a;
             arenaB = b;
             gameObjectRef = objRef;
+            spline = gameObjectRef.GetComponent<SplineContainer>().Spline;
             // Paths are only hidden if they are connected to a hidden arena
             isHidden = false;
         }
@@ -95,7 +105,6 @@ public class MapGenerator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
     }
 
     void GenerateMap()
@@ -168,9 +177,15 @@ public class MapGenerator : MonoBehaviour
             CreatePath(arenas[i], secondClosestArena, pathCount.ToString());
             pathCount++;
         }
-
+        
+        foreach (Path path in paths)
+        {
+            RepositionEntrance(path, "A");
+            RepositionEntrance(path, "B");
+        }
+        
         // Space out all the path points so they dont overlap!
-        //StartCoroutine(SpaceOutPaths());
+        SpaceOutPaths();
     }
 
 
@@ -218,7 +233,7 @@ public class MapGenerator : MonoBehaviour
         {
             // Add a capsule collider to define the bounds of the arena
             CapsuleCollider collider = arena.AddComponent<CapsuleCollider>();
-            collider.radius = arena.transform.localScale.x / (scaleFactor * 60); // Set the radius
+            collider.radius = arena.transform.localScale.x / (scaleFactor * 60) + 0.25f; // Set the radius
             collider.height = 100f; // Set the height
             collider.center = new Vector3(0, 10, 0); // Center the collider on the arena
             collider.isTrigger = true; // Set the collider to be a trigger so players can fall through
@@ -228,17 +243,22 @@ public class MapGenerator : MonoBehaviour
                 arena.name = "Arena_" + numberforname;
             }
             // Create an Arena instance to hold the arena's data
-            Arena arenaData = new Arena(arena.transform.position, arena);
+            Arena arenaData = new Arena(arena.transform.position, arena, scaleFactor);
             arenas.Add(arenaData); // Store reference to the created arena
+
+            arena.tag = "Arena"; // Set the tag of the arena to "Arena" for easy reference
+
             return true; // No overlap, return true
         }
     }
 
     // Overload of CreateArena that takes in Vector3 min and max for a random position,
     // Then calls the original version on a random position within the box created by the min and max
+    // The min and max are the bounds of the area where the arena can spawn
     bool CreateArena(Vector3 min, Vector3 max, String numberForName)
     {
         // Create the arena, and check if the arena was successfully created
+        // This call doesn't pass a scalefactor, so it defaults to 1f
         if ( CreateArena(
             new Vector3(Random.Range(min.x, max.x), Random.Range(min.y, max.y), Random.Range(min.z, max.z)),
             numberForName
@@ -344,6 +364,7 @@ public class MapGenerator : MonoBehaviour
             // The position on the spline based on the place value
             Vector3 midPos = splineContainer.EvaluatePosition(spline, place);
 
+            
             // Get a vector perpedicular to the spline's x and z
             // Vector3 moveVector = Vector3.Cross(Vector3.down, betweenArenaVector.normalized); IDK WHY this isn't working, it's making loops in the paths
             Vector3 moveVector = Vector3.zero;
@@ -359,7 +380,8 @@ public class MapGenerator : MonoBehaviour
 
             // Move the position using the vector, random magnitude
             // float curveStrength = Random.Range(30, 50);
-            midPos += moveVector * Random.Range(30, 50);
+            //midPos += moveVector * Random.Range(20, 40);
+            
 
             // Add the place on spline and the position into the newKnotPositions array
             knotPositions[i] = (place, midPos);
@@ -392,6 +414,21 @@ public class MapGenerator : MonoBehaviour
         //pathPoints.Add(beforeAknot);
         //pathPoints.Add(beforeBknot);
         
+        CleanUpPath(spline, splineContainer);
+        
+        
+        // Add a mesh to this path
+        CreatePathMesh(splineContainer);
+
+        // Create a Path instance to hold the path's data
+        Path pathData = new Path(arenaA, arenaB, splineContainer.gameObject);
+
+        paths.Add(pathData); // Store reference to the created path
+    }
+
+
+    void CleanUpPath(Spline spline, SplineContainer splineContainer)
+    {
         // Clean up knots that are too sharp
         // Shouldn't take more than 50 tries
         int attempts = 0;
@@ -421,19 +458,10 @@ public class MapGenerator : MonoBehaviour
             }
             attempts++;
         }
-        
-        
-        // Add a mesh to this path
-        CreatePathMesh(splineContainer);
-
-        // Create a Path instance to hold the path's data
-        Path pathData = new Path(arenaA, arenaB, splineContainer.gameObject);
-
-        paths.Add(pathData); // Store reference to the created path
     }
 
 
-    // Adds the mesh to the input path game object
+    // Adds the mesh and mesh collider to the input path game object
     void CreatePathMesh(SplineContainer container)
     {
         // Temporary implementation, just used to make a visible path rn!!
@@ -463,41 +491,177 @@ public class MapGenerator : MonoBehaviour
         // or if you turn it off and on here, so that's what this is for
         splineExtrude.enabled = false;
         splineExtrude.enabled = true;
+
+        // Add a mesh collider and set it to the generated mesh
+        MeshCollider meshCollider = container.gameObject.AddComponent<MeshCollider>();
+        
     }
 
-    IEnumerator SpaceOutPaths()
+    // Repositions the entrance of the path to be farther from other entrances, so they don't overlap/get too close
+    void RepositionEntrance(Path path, string whichEntrance = "A")
+    {
+        BezierKnot entranceKnot;
+        Arena arena = null;
+        int knotIndex;
+        int secondaryKnotIndex; // Used for the knot that makes the path face the arena
+        // The passed whichEntrance string decides if we're checking the first knot or the last
+        switch (whichEntrance)
+        {
+            case "A":
+                knotIndex = 0;
+                secondaryKnotIndex = 1;
+                arena = path.arenaA;
+                break;
+            case "B":
+                Spline spline = path.gameObjectRef.GetComponent<SplineContainer>().Spline;
+                knotIndex = spline.Count-1;
+                secondaryKnotIndex = spline.Count-2;
+                arena = path.arenaB;
+                break;
+            default:
+                Debug.LogWarning("Invalid entrance specified for repositioning. Must be 'A' or 'B'.");
+                return;
+        }
+        // Set the knot to whatever index we got from whichEntrance (first or last)
+        entranceKnot = path.gameObjectRef.GetComponent<SplineContainer>().Spline[knotIndex];
+
+        // Get necessary references
+        Vector3 entrancePos = entranceKnot.Position; // Knot's position
+        Vector3 directionFromArena = (entrancePos - arena.position).normalized; // Dir from arena center to the knot
+        float angle = Mathf.Atan2(directionFromArena.z, directionFromArena.x); // Float angle value of the direction (in radians)
+
+        // Initialize sweep angle in the positive direction
+        float posAngle = angle;
+        bool foundAngle = false;
+        int attempts = 0;
+        // Sweep angle to +90 degrees (from the path entrance) in increments of 1 degree, and raycast in that direction to check for other entrances
+        while (attempts < 90 && !foundAngle)
+        {
+            posAngle += Mathf.Deg2Rad * 1; // Increment the angle by 1 degree
+            attempts++; // Increment attempts
+
+            // Get the direction vector from the arena based on the angle
+            directionFromArena = new Vector3(Mathf.Cos(posAngle), 0, Mathf.Sin(posAngle));
+
+            // Raycast from the arena
+            RaycastHit[] hits = Physics.RaycastAll(arena.position, directionFromArena, arena.gameObjectRef.transform.localScale.x + 50f);
+            foreach (RaycastHit hit in hits)
+            {
+                // If we hit a path's collider other than the entrance collider
+                if (hit.collider.gameObject.CompareTag("Path") && hit.collider.gameObject != path.gameObjectRef)
+                {
+                    Debug.Log("GOINGPOS " + "PATH " + path.gameObjectRef.name + "HIT COLL ->" + hit.collider.gameObject.name);
+                    foundAngle = true; // there's another entrance in this direction
+                    break; // Exit the loop
+                }
+            }
+        }
+
+        // Initialize sweep angle in the negative direction
+        float negAngle = angle;
+        foundAngle = false;
+        attempts = 0;
+        // Sweep angle to -90 degrees (from the path entrance) in increments of 1 degree, and raycast in that direction to check for other entrances
+        while (attempts < 90 && !foundAngle)
+        {
+            negAngle -= Mathf.Deg2Rad * 1; // decrement the angle by 1 degree
+            attempts++; // Increment attempts
+
+            // Get the direction vector from the arena based on the angle
+            directionFromArena = new Vector3(Mathf.Cos(negAngle), 0, Mathf.Sin(negAngle));
+
+            // Raycast from the arena
+            RaycastHit[] hits = Physics.RaycastAll(arena.position, directionFromArena, arena.gameObjectRef.transform.localScale.x + 50f);
+            foreach (RaycastHit hit in hits)
+            {
+                // If we hit a path's collider other than the entrance collider
+                if (hit.collider.gameObject.CompareTag("Path") && hit.collider.gameObject != path.gameObjectRef)
+                {
+                    Debug.Log("GOINGNEG" + " PATH " + path.gameObjectRef.name + "HIT COLL ->" + hit.collider.gameObject.name);
+                    foundAngle = true; // there's another entrance in this direction
+                    break; // Exit the loop
+                }
+            }
+        }
+
+        // Now decide on the new angle of the entrance
+        float newAngle = 0f;
+        // compare the final pos and neg angles, and choose the one that is farther from other path entrances
+        if (Mathf.Abs(posAngle - angle) > Mathf.Abs(negAngle - angle))
+        {
+            newAngle = Random.Range(angle + Mathf.Deg2Rad * 5, posAngle);
+        }
+        else
+        {
+            newAngle = Random.Range(angle - Mathf.Deg2Rad * 5, negAngle);
+        }
+        
+        // Get a point in the new direction to get 
+        Vector3 newDirection = new Vector3(Mathf.Cos(newAngle), 0, Mathf.Sin(newAngle));
+        Vector3 newPoint = arena.position + newDirection * (arena.gameObjectRef.transform.localScale.x);
+                
+        entranceKnot.Position = arena.arenaBounds.ClosestPoint(newPoint);
+        Debug.Log(
+            angle + "<-ANGLE \n POSANGLE->" + posAngle + " " + Mathf.Abs(posAngle - angle) + " " + (angle + 90*Mathf.Deg2Rad).ToString() + " \n NEGANGLE->" + negAngle + " " + Mathf.Abs(negAngle - angle) + " " + (angle - 90*Mathf.Deg2Rad).ToString() + " \n RADIUS-> " + arena.arenaBounds.radius + " \n " + newPoint + "<-NEWPOINT \n ARENA->" + arena.gameObjectRef.name + " \n PATH->" + path.gameObjectRef.name + " \n CLOSEST->" + arena.arenaBounds.ClosestPoint(newPoint)
+            );
+        // Gotta set knot to make it actually move the knot
+        path.spline.SetKnot(knotIndex, entranceKnot);
+
+        // Do the same for the secondary knot, slightly farther out
+        BezierKnot secondaryKnot = path.spline[secondaryKnotIndex];
+        secondaryKnot.Position = newPoint + newDirection * 25f;
+        path.spline.SetKnot(secondaryKnotIndex, secondaryKnot);
+    }
+
+    void SpaceOutPaths()
     {
         bool pointsAllSpaced = false;
         int attempts = 0;
-        while (!pointsAllSpaced && attempts < 10000)
+        while (!pointsAllSpaced && attempts < 10)
         {
-            yield return new WaitForSeconds(.1f);
+            // yield return new WaitForSeconds(.1f);
             attempts++;
             pointsAllSpaced = true;
-            // Loop through all points, and loop through all points for each point
-            for (int i = 0; i < pathMidpoints.Count; i++)
+            // Loop through all paths
+            for (int i = 0; i < paths.Count; i++)
             {
-                for (int j = 0; j < pathMidpoints.Count; j++)
+                // Loop through this path's points (skip the 2 points from each end)
+                for (int knotIndex = 2; knotIndex < paths[i].spline.Count-2; knotIndex++)
                 {
                     // Get knots from list
-                    BezierKnot knot = pathMidpoints[i];
-                    BezierKnot otherKnot = pathMidpoints[j];
-                    // Get their positions
-                    Vector3 pos = pathMidpoints[i].Position;
-                    Vector3 otherPos = pathMidpoints[j].Position;
+                    BezierKnot knot = paths[i].spline[knotIndex];
+                    // Check a sphere with 75 radius on the point if there are colliders overlapping
+                    Collider[] hitColliders = Physics.OverlapSphere(knot.Position, 75f);
 
-                    Vector3 betweenVector = otherPos - pos;
-
-                    // Check if they're too close
-                    if (pos != otherPos && betweenVector.magnitude < 50f)
+                    foreach (var hitCollider in hitColliders)
                     {
-                        Debug.Log("Spacing out " + knot + " and " + otherKnot + " at distance " + Vector3.Distance(pos, otherPos));
-                        // Set points spaced false and space them apart
-                        pointsAllSpaced = false;
-                        
-                        Vector3 newPos = pos;
-                        newPos -= betweenVector.normalized * (50 - betweenVector.magnitude) / 50;
-                        knot.Position = newPos;
+                        // Don't check the current path
+                        if (hitCollider.gameObject != paths[i].gameObjectRef) {
+                            // Set points spaced false and space them apart
+                            pointsAllSpaced = false;
+
+                            // If it was a path, loop through its points
+                            if (hitCollider.gameObject.tag == "Path")
+                            {
+                                Spline otherSpline = hitCollider.gameObject.GetComponent<SplineContainer>().Spline;
+                                for (int otherKnotIndex = 0; otherKnotIndex < otherSpline.Count; otherKnotIndex++)
+                                {
+                                    // Get their positions
+                                    Vector3 pos = knot.Position;
+                                    Vector3 otherPos = otherSpline[otherKnotIndex].Position;
+
+                                    Vector3 betweenVector = otherPos - pos;
+
+                                    Debug.Log("Spacing out " + knot + " and " + otherSpline[otherKnotIndex] + " at distance " + Vector3.Distance(pos, otherPos));
+                                    
+                                    Vector3 newPos = pos;
+                                    newPos -= betweenVector.normalized * (50 - betweenVector.magnitude) / 50;
+                                    knot.Position = newPos;
+
+                                    paths[i].spline.SetKnot(knotIndex, knot);
+                                }
+                            }
+                        }
                     }
                 }
             }
@@ -509,5 +673,6 @@ public class MapGenerator : MonoBehaviour
         // Draws the bounds of the map's generation area (between minBounds and maxBounds)
         Gizmos.color = Color.yellow;
         Gizmos.DrawWireCube(Vector3.Lerp(minBounds, maxBounds, 0.5f), maxBounds - minBounds);
+
     }
 }
